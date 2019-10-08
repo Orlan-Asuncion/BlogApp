e568fa74c94d3144fb43a60237c50e5afc6936bc
Orlan-Asuncion <48038688+Orlan-Asuncion@users.noreply.github.com>
2019-10-07 21:08:17 -0400

    add file app.js

--- app.js
+++ app.js
@@ -21,10 +21,42 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function(req, res){
 res.redirect("/blogs");
 });
+//index route
 app.get("/blogs", function(req, res){
-    res.render("index");
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("ERROR!");
+        } else {
+            res.render("index", {blogs:blogs});
+        }
+    });
 });
-
+//new route
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+//create route
+app.post("/blogs", function(req, res){
+    //create blog
+    Blog.create(req.body.blogSchema, function(err, newBlog){
+            if(err){
+                res.render("new");
+            } else {
+                //then, res.redirect to the index
+                res.redirect("/blogs");
+            }
+    });    
+});
+//show route
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
